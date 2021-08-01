463e883ebcc38485fa7e9b188ba780de8454f7f9
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-01 23:25:01 +1200

    fixed eslint errors

--- dynamicrisk/components/JobCard.js
+++ dynamicrisk/components/JobCard.js
@@ -3,16 +3,14 @@ import Row from 'react-bootstrap/Row'
 import Input from './Input'
 
 export default function JobCard() {
-  {
-    return (
-      <>
-        <Container>
-          <Row>
-            <h1>Job Title</h1>
-          </Row>
-          <Input type="email" placeholder="richmond@gmail.com" />
-        </Container>
-      </>
-    )
-  }
+  return (
+    <>
+      <Container>
+        <Row>
+          <h1>Job Title</h1>
+        </Row>
+        <Input type="email" placeholder="richmond@gmail.com" />
+      </Container>
+    </>
+  )
 }
