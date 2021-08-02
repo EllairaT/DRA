df2b9b569081a54e1040a25ea60c5d6ef6c672a5
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-02 18:28:50 +1200

    changed linebreak LF to CRLF

--- components/newJobForm.js
+++ components/newJobForm.js
@@ -9,14 +9,18 @@ export default function newDRAForm() {
           <h1>Create new Assessment</h1>
         </Row>
         <Form>
-          <label>Job Site:</label><Input type="text" placeholder="Name of Location" />
-          <label>Job Address:</label><Input type="text" placeholder="Address of Location" />
-          <label>Job Site:</label><Input type="text" placeholder="Name of Location" />
+          <label>Job Site:</label>
+          <Input type="text" placeholder="Name of Location" />
+          <label>Job Address:</label>
+          <Input type="text" placeholder="Address of Location" />
+          <label>Job Site:</label>
+          <Input type="text" placeholder="Name of Location" />
 
           <Row>
             <Col>
               <label>Phone:</label>
-              <Input type="tel" placeholder="Phone Number of Location" /></Col>
+              <Input type="tel" placeholder="Phone Number of Location" />
+            </Col>
             <Col>
               <label>Date:</label>
               <Input type="date" />
@@ -28,9 +32,7 @@ export default function newDRAForm() {
           </Row>
 
           <Button as="input" type="submit" value="submit" />
-
         </Form>
-
       </Container>
     </>
   )
