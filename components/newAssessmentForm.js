ba4d3018bc983fb53bd7882266c3b3469ddf0c97
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-04 02:35:03 +1200

    installed mongodb

--- components/newAssessmentForm.js
+++ components/newAssessmentForm.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import Input from './Input'
 
 export default function NewDRAForm(props) {
-
   const [job, setJob] = useState({
     JobSite: '',
     JobAddress: '',
@@ -16,16 +15,16 @@ export default function NewDRAForm(props) {
   const onSubmit = (e) => {
     e.preventDefault()
     // Needed to be added to database to store
-    alert(`Submitting ${job.JobSite}, ${job.JobAddress}, ${job.JobSiteDescription}, ${job.Phone}, ${job.Date}, ${job.Time}`)
-
+    alert(
+      `Submitting ${job.JobSite}, ${job.JobAddress}, ${job.JobSiteDescription}, ${job.Phone}, ${job.Date}, ${job.Time}`
+    )
   }
   const inputsHandler = (e) => {
     // update the attributes in object
-    const {name} = e.target
-    const {value} = e.target
+    const { name } = e.target
+    const { value } = e.target
     job[name] = value
     setJob(job)
-
   }
 
   return (
@@ -37,31 +36,22 @@ export default function NewDRAForm(props) {
         <Form>
           Job Site:
           <Input type="text" placeholder="Name of Location" name="JobSite" onChange={inputsHandler} />
-          
-
           Job Address:
           <Input type="text" placeholder="Address of Location" name="JobAddress" onChange={inputsHandler} />
-          
-
           Job Site Description:
           <Input type="text" placeholder="Description of Location" name="JobSiteDescription" onChange={inputsHandler} />
-          
-
           <Row>
             <Col>
               Phone:
               <Input type="tel" placeholder="Phone Number of Location" name="Phone" onChange={inputsHandler} />
-              
             </Col>
             <Col>
               Date:
               <Input type="date" name="Date" onChange={inputsHandler} />
-              
             </Col>
             <Col>
               Time:
               <Input type="time" name="Time" onChange={inputsHandler} />
-              
             </Col>
           </Row>
           <Button as="input" onClick={onSubmit} value="submit" />
