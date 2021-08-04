e077227ba2f310a52a9d9ab3e43560460e728f98
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-05 03:34:16 +1200

    added bootstrap icons to sidebar

--- components/Navi.js
+++ components/Navi.js
@@ -1,13 +1,19 @@
 import Nav from 'react-bootstrap/Nav'
-import customnav from './Navi.module.css'
-
+import NavCSS from './Navi.module.css'
+import { CalendarPlus, JournalPlus, ClockHistory, HouseDoor } from 'react-bootstrap-icons'
 export default function Navi() {
   return (
     <>
-      <Nav className={customnav}>
-        <Nav.Link href="/">Dashboard</Nav.Link>
-        <Nav.Link href="/createAssessment">Create New Job</Nav.Link>
-        <Nav.Link href="/scheduleAssessment">Schedule New Job</Nav.Link>
+      <Nav className={NavCSS.customnav}>
+        <Nav.Link href="/" className={NavCSS.icons}>
+          <HouseDoor size={30} />
+        </Nav.Link>
+        <Nav.Link href="/createAssessment" className={NavCSS.icons}>
+          <JournalPlus size={30} />
+        </Nav.Link>
+        <Nav.Link href="/scheduleAssessment" className={NavCSS.icons}>
+          <CalendarPlus size={30} />
+        </Nav.Link>
         {/* <Nav.Link eventKey="link-2">Link</Nav.Link> */}
       </Nav>
     </>
