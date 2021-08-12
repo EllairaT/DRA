324c074a04fab580e6a3ef2c8980f6b41048f109
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-12 22:47:57 +1200

    update

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,25 @@
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
+import Image from 'next/image'
+import { Row, Col, Card, Form } from 'react-bootstrap'
 import Input from '../components/Input'
+import logoimg from '../1.png'
 
 function Login() {
     return (
         <>
             <Card className="loginCard">
-                <Form>
-                    <Card.Body className="">This is some text within a card body.</Card.Body>
-                    <Input label="Username" type="email" placeholder="example@outlook.com" />
-                    <Input label="Password" type="password" placeholder="Enter password" />
-                    <Form.Text>Never tell anyone your password. </Form.Text>
-                </Form>
+                <Row>
+                    <Col xs={6}>
+                        <Image src={logoimg} alt="Img" />
+                    </Col>
+                    <Col>
+                        <Form>
+                            <Card.Body className="">This is some text within a card body.</Card.Body>
+                            <Input label="Username" type="email" placeholder="example@outlook.com" />
+                            <Input label="Password" type="password" placeholder="Enter password" />
+                            <Form.Text>Never tell anyone your password. </Form.Text>
+                        </Form>
+                    </Col>
+                </Row>
             </Card>
         </>
     )
