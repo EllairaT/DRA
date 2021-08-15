8e5dc0595246ffa966027658a662351598ebdb73
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-16 02:56:13 +1200

    center login layout

--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,62 @@
-
 import { Row, Col, Card, Form } from 'react-bootstrap'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Input from '../components/Input'
 import logoimg from '../1.png'
-import loginImg from './login.module.css'
+import login from './login.module.css'
 
 function Login() {
-    return (
-        <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-            <Card className="loginCard p-0 m-0">
-                <Row>
-                    <Col md={6}>
-                        <div className="align-left">
-                            <div className={loginImg.unsetImg}>
-                                <Image src={logoimg} alt="Img" layout="fill" className={loginImg.customImg} />
-                            </div>
-                        </div>
-                    </Col>
-                    <Col >
-                        <Card.Body className="m-0">Login
-                            <Form>
-                                <Input label="Username" type="email" placeholder="example@outlook.com" />
-                                <Input label="Password" type="password" placeholder="Enter password" />
-                                <Form.Text>Never tell anyone your password. </Form.Text>
-                            </Form>
-                        </Card.Body>
-                    </Col>
-                </Row>
-            </Card>
-        </div >
-    )
+  const [details, setDetails] = useState({
+    userName: '',
+    userPassword: ''
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const inputHandler = (e) => {
+    const { name } = e.target
+    const { value } = e.target
+    details[name] = value
+    setDetails(details)
+  }
+
+  return (
+    <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+      <Card className="container p-0 mx-auto">
+        <div className="d-flex">
+          <Card className={login.left}>
+            <Image src={logoimg} alt="Main Image" />
+          </Card>
+          <Card className={login.right}>
+            <div class="my-auto mx-md-5 px-md-5 right">
+              <Form>
+                <div className="h5">Login to your account</div>
+                <Form.Group controlId="loginDetails">
+                  <Input
+                    label="Username"
+                    type="email"
+                    name="userName"
+                    placeholder="example@outlook.com"
+                    onChange={inputHandler}
+                  />
+                  <Input
+                    label="Password"
+                    type="password"
+                    name="userPassword"
+                    placeholder="Enter password"
+                    onChange={inputHandler}
+                  />
+                  <Form.Text>Never tell anyone your password. </Form.Text>
+                </Form.Group>
+              </Form>
+            </div>
+          </Card>
+        </div>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
