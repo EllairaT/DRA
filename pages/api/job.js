58ddc20f823dbd425876ebe6018625b931ca511a
fengv1976 <fengv1976@gmail.com>
2021-08-16 17:15:44 +1200

    Comments and name fixes
    Tried getting the api working but having issues with database connection

--- pages/api/job.js
+++ pages/api/job.js
@@ -1,16 +1,35 @@
 import dbConnect from '../../utils/dbConnect'
 import Job from '../../models/job.model'
 
-dbConnect()
+dbConnect();
 
 const jobReqs = async (req, res) => {
     const { method } = req
 
+    // testing url http://localhost:3000/api/job
+
+    // testing for post
+    // {
+    //     "date": "2015-11-11",
+    //     "site": "NZ",
+    //     "siteAddress": "1 city",
+    //     "siteType": "construction",
+    //     "phone": "111111111",
+    //     "inspector": "Geoff",
+    //     "notes": "looks like a level 10",
+    //     "siteTags": ["machine", "tall stuff"],
+    //     "assessments": ["12","13"],
+    //     "createdAt": "2019-01-01"
+    // }
+
+
     switch (method) {
         case 'GET':
             try {
-                const notes = await Job.find({})
-                res.status(200).json({ success: true, data: notes })
+                // get all jobs
+                const jobs = await Job.find()
+
+                res.status(200).json({ success: true, data: jobs })
             } catch (error) {
                 res.status(400).json({ success: false })
             }
@@ -18,7 +37,9 @@ const jobReqs = async (req, res) => {
 
         case 'POST':
             try {
+                // create new job
                 const job = await Job.create(req.body)
+
                 res.status(200).json({ success: true, data: job })
             } catch (error) {
                 res.status(400).json({ success: false })
