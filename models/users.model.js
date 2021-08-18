6b406969781b08c010d2ed5c3ce63fd63a701e9d
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-19 01:30:15 +1200

    modify user model

--- models/users.model.js
+++ models/users.model.js
@@ -4,13 +4,17 @@ const mongoose = Mongoose
 const { Schema, Model } = mongoose
 
 const userSchema = new Schema({
-  name: {
+  username: {
     type: String,
-    required: true
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true //index set to true to optimise queries that use username and password fields
   },
   email: {
     type: String,
-    required: true
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true
   },
   password: {
     type: String,
@@ -19,7 +23,9 @@ const userSchema = new Schema({
   since: {
     type: Date,
     default: Date.now
-  }
+  },
+  hash: String,
+  salt: String
 })
 
 const User = new Model('User', userSchema)
