eb8b700b192ffcc06a5f27fe87d112ede6a2cca1
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-20 04:55:23 +1200

    revert changes, going back to mongo

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,6 @@
 import connectToDatabase from '../../lib/dbConnect'
-import connectToAzure from '../../lib/azureConnect'
+
 connectToDatabase()
-connectToAzure()
 
 export default async (req, res) => {
   res.json({ test: 'test' })
