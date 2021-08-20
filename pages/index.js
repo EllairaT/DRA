531250ff0a460fd4adc0a747ecafb9508133e416
SJ-47 <stjohnlyon2000@gmail.com>
2021-08-20 23:35:41 +1200

    Barebones Homepage
    Href links working, will add styling and other components that are on the mock up homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import { Container, Row, Col } from 'react-bootstrap'
 import JobCard from '../components/JobCard'
 import Navi from '../components/Navi'
 import Login from './login'
+import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button'
+import { Container, CardGroup, Card, Row, Col } from 'react-bootstrap'
+import createAssessment from '../pages/createAssessment'
 
 /**
  * Entry point of the app.
@@ -10,10 +13,32 @@ import Login from './login'
 function Home() {
   return (
     <>
-      {/* <h1>Dynamic Risk Assessment </h1>
-      <JobCard /> */}
+    <Container>
+      <Card>
+<h1 >No Scheduled Jobs to Show Today</h1>
+<h4>What would you like to do instead?</h4>
+<br></br>
+        <Col>
+          <Row>              
+            <Button href="./scheduleAssessment">Schedule Assessment</Button>{'      '}
+          </Row>
+        </Col>
+      <br></br>
+      <Col>
+        <Row>      
+        <Button href="./createAssessment">Create New Assessment</Button>{'      '}
+</Row>
+</Col>
+      </Card>
+    </Container>
     </>
   )
 }
 
 export default Home
+
+
+
+/* <h1>Dynamic Risk Assessment </h1>
+      <JobCard /> */
+
