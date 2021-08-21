59101d07aa4be6a0260fe74d2527cd2348a53626
SJ-47 <stjohnlyon2000@gmail.com>
2021-08-22 11:42:12 +1200

    Update Index.js
    Search Input

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,25 @@ import Navi from '../components/Navi'
 import Login from './login'
 import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap/Button'
-import { Container, CardGroup, Card, Row, Col } from 'react-bootstrap'
+import { Container, Form, FormControl, InputGroup, CardGroup, Card, Row, Col } from 'react-bootstrap'
 import createAssessment from '../pages/createAssessment'
+import { Search } from 'react-bootstrap-icons'
+import Input from '../components/Input'
 
 /**
  * Entry point of the app.
  * @Category Pages
  */
+
 function Home() {
   return (
     <>
     <Container>
       <Card>
+      <InputGroup>
+  <InputGroup.Text>Search</InputGroup.Text>
+  <Form.Control type="text" />
+</InputGroup>     
 <h1 >No Scheduled Jobs to Show Today</h1>
 <h4>What would you like to do instead?</h4>
 <br></br>
