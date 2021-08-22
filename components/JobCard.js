08fc3e3c5710cdd5c3e71d1828d5997ad08bda21
fengv1976 <fengv1976@gmail.com>
2021-08-22 16:30:36 +1200

    added some basic icons to jobcard

--- components/JobCard.js
+++ components/JobCard.js
@@ -1,4 +1,5 @@
 import Container from 'react-bootstrap/Container'
+import { Clock, Phone, Geo } from 'react-bootstrap-icons'
 import Row from 'react-bootstrap/Row'
 import Input from './Input'
 
@@ -6,9 +7,18 @@ const JobCard = ({job}) => {
   return (
     <>
       <Container>
-          <h1>{job.site}</h1>
-          <p>{job.siteAddress}</p>
-          <p>{job.date}</p>
+
+        <h1>{job.site} | {job.siteType}</h1>
+
+        <Geo size={20} />
+        <p>{job.siteAddress}</p>
+
+        <Phone size={20} />
+        <p>{job.phone}</p>
+
+        {/* have to be view in dd-mm hh-mm format */}
+        <Clock size={20} />
+        <p>{job.date}</p>
       </Container>
     </>
   )
