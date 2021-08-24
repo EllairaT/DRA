13e69f8e40e50a741db567b23cecfe158e4d23fe
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-08-24 21:54:49 +1200

    clean up homepage

--- pages/index.js
+++ pages/index.js
@@ -7,45 +7,57 @@ import { Container, Form, FormControl, InputGroup, CardGroup, Card, Row, Col } f
 import createAssessment from '../pages/createAssessment'
 import { Search } from 'react-bootstrap-icons'
 import Input from '../components/Input'
-
+import { getSession } from 'next-auth'
 /**
  * Entry point of the app.
  * @Category Pages
  */
 
 function Home() {
+  const [session] = getSession()
+
+  var noJobContent = (
+    <>
+      <h1>No Scheduled Jobs to Show Today</h1>
+      <h4>What would you like to do instead?</h4>
+      <Row>
+        <Col md={6}>
+          <Button href="./scheduleAssessment">Schedule Assessment</Button>
+          {'      '}
+        </Col>
+
+        <Col md={6}>
+          <Button href="./createAssessment">Create New Assessment</Button>
+          {'      '}
+        </Col>
+      </Row>
+    </>
+  )
+
   return (
     <>
-    <Container>
-      <Card>
-      <InputGroup>
-  <InputGroup.Text>Search</InputGroup.Text>
-  <Form.Control type="text" />
-</InputGroup>     
-<h1 >No Scheduled Jobs to Show Today</h1>
-<h4>What would you like to do instead?</h4>
-<br></br>
-        <Col>
-          <Row>              
-            <Button href="./scheduleAssessment">Schedule Assessment</Button>{'      '}
-          </Row>
-        </Col>
-      <br></br>
-      <Col>
-        <Row>      
-        <Button href="./createAssessment">Create New Assessment</Button>{'      '}
-</Row>
-</Col>
-      </Card>
-    </Container>
+      <Container>
+        <Card>
+          <InputGroup>
+            {/* add search icon */}
+            <InputGroup.Text>Search</InputGroup.Text>
+            <FormControl type="text" />
+          </InputGroup>
+
+          {/* check if there is session, if no session then show no job content */}
+          {!session && <>{noJobContent}</>}
+          {session && (
+            <>
+              <JobCard />
+            </>
+          )}
+        </Card>
+      </Container>
     </>
   )
 }
 
 export default Home
 
-
-
 /* <h1>Dynamic Risk Assessment </h1>
-      <JobCard /> */
-
+ */
