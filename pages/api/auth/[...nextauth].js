3358a69fce59b42ff98dd8a35f3a2993935f5f78
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-09-20 18:59:30 +1200

    improvements

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,13 +13,6 @@ const options = {
         password: { type: 'password' }
       },
       async authorize(credentials) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-
         //connect to MongoDB
         connectToDatabase()
         const res = await User.findOne({ email: credentials.email })
