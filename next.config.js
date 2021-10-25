23f7dc42d3688f5bcc3342377b350fa3fca68a24
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-10-25 16:38:31 +1300

    remove distDir override

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 const withTM = require('next-transpile-modules')(['babel-eslint']) // pass the modules you would like to see transpiled
 
 module.exports = withTM()
-module.exports = {
-  distDir: 'build'
-}
